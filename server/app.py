b9344a1dfb4f5463bed60a42d0efb0cb34e920c6
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-07-03 15:30:27 +0300

    complete lab

--- server/app.py
+++ server/app.py
@@ -28,16 +28,45 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        if 'user_id' in session:
+            user_id = session['user_id']
+            user = db.session.query(User).filter(User.id == user_id).first()
+            if user:
+                return user.to_dict(), 200
+        return {}, 204
 
 class Login(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+
+        if not username or not password:
+            return {'error': 'Username and password are required'}, 400
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+
+        return {'error': '401 Unauthorized'}, 401
 
 class Logout(Resource):
-    pass
+    
+    def delete(self):
+        if 'user_id' in session:
+            session.pop('user_id')
+        return {}, 204
+
+
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
